6c8b83c6f486f23d02ad8efc3b153558fc5477da
AgentHitmanFaris <anonymouslgg@gmail.com>
2025-11-24 22:36:15 +0800

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+// build.gradle.kts (Project: NC_Score_Beta)
+plugins {
+    id("com.android.application") version "8.13.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
+
+    // ADD THIS LINE BELOW (matches your screenshot):
+    id("com.google.gms.google-services") version "4.4.4" apply false
+}
